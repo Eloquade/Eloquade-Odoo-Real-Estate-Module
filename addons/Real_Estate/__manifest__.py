d1ed4fc2674add893238b1cad97f11eca1da1dea
Eloquade <perez.lawrence08@gmail.com>
2025-09-17 14:57:50 +0800

    Added Icon

--- addons/Real_Estate/__manifest__.py
+++ addons/Real_Estate/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '18.0.1.0.0',
     'summary': 'Manage real estate properties, offers, and sales',
     'description': 'A module to manage real estate properties, offers, and sales in Odoo.',
-    'author': 'Your Name or Company',
+    'author': 'Lawrence',
     'website': 'https://yourcompany.com',
     'category': 'Real Estate',
     'depends': ['base'],
@@ -12,8 +12,9 @@
         'views/real_estate_property_type_views.xml',
         'views/real_estate_tag_views.xml',
         'security/ir.model.access.csv',
-        
+
     ],
+    'images': ['static/description/icon.jpg'],
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
