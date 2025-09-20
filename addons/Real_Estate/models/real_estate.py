18dec6075d21675bb8c4fac2300ad8379bdd68a5
Eloquade <perez.lawrence08@gmail.com>
2025-09-20 17:59:41 +0800

    Added Onchange

--- addons/Real_Estate/models/real_estate.py
+++ addons/Real_Estate/models/real_estate.py
@@ -25,7 +25,20 @@ class RealEstate(models.Model):
     tag_ids = fields.Many2many('real.estate.tag', string='Tags')
     property_type_id = fields.Many2one('real.estate.property.type', string='Property Type')
 
+
     @api.depends('living_area', 'garden_area')
     def _compute_area(self):
         for record in self:
             record.area = record.living_area * record.garden_area
+            
+    
+    @api.onchange('date_available')
+    def _onchange_date_available(self):
+        if self.date_available and self.date_available < fields.Date.today():
+            self.date_available = fields.Date.today()
+            return {
+                'warning': {
+                    'title': "Invalid Date",
+                    'message': "The available date cannot be in the past. It has been reset to today.",
+                }
+            }
