8961b684614882835a4e331136399566a351c263
Eloquade <perez.lawrence08@gmail.com>
2025-09-24 21:40:21 +0800

    Added Action Buttons

--- addons/Real_Estate/models/real_estate_property_type.py
+++ addons/Real_Estate/models/real_estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class RealEstatePropertyType(models.Model):
     _name = 'real.estate.property.type'
@@ -7,3 +7,21 @@ class RealEstatePropertyType(models.Model):
     name = fields.Char(string='Name', required=True)
     description = fields.Text(string='Description')
     active = fields.Boolean(string='Active', default=True)
+    property_ids = fields.One2many('real.estate', 'property_type_id', string='Properties')
+    property_count = fields.Integer(string='Property Count', compute='_compute_property_count')
+
+    def action_open_properties(self):
+        return {
+            'name': 'Properties',
+            'type': 'ir.actions.act_window',
+            'res_model': 'real.estate',
+            'view_mode': 'list,form',
+            'target': 'current',
+            'domain': [('property_type_id', '=', self.id)],
+            'context': {'default_property_type_id': self.id},
+        }
+    
+    @api.depends('property_ids')
+    def _compute_property_count(self):
+        for record in self:
+            record.property_count = len(record.property_ids)
